96d6c3c836b780bb9f2598cfccc52ab053f4b9aa
Rolland <Rollandcampbell@msn.com>
2019-08-17 14:02:58 -0600

    combined attack buttons into 1 formula

--- game.js
+++ game.js
@@ -15,43 +15,58 @@ let werewolf = {
   inventory: []
 }
 
+//later use
 let player = {
   name: "Conan",
   health: 100,
   inventory: []
 }
 
+//reset button set helth to 100, clear out array of item
 function reset() {
   health = 100;
+  console.log(werewolf.inventory[]);
+  // werewolf.inventory[];
   update()
 }
 
-function slap() {
-  health--;
-  update()
-}
+//original attack buttons
+// function slap() {
+//   health--;
+//   update()
+// }
 
-function punch() {
-  health -= 5;
-  update()
-}
+// function punch() {
+//   health -= 5;
+//   update()
+// }
 
-function kick() {
-  health -= 10;
-  update()
-}
+// function kick() {
+//   health -= 10;
+//   update()
+// }
 
 function update() {
   let decreaseHealth = document.getElementById("healthEnemy")
   decreaseHealth.innerHTML = health.toString();
 }
 
+function attackButton(inputAttack) {
+  if (inputAttack == 'slap') {
+    health--;
+    update()
+  } else if (inputAttack == 'punch') {
+    health -= 5;
+    update()
+  } else if (inputAttack == 'kick') {
+    health -= 10;
+    update()
+  }
+}
+
+//move item from obj list to inventory
 function giveItem(input) {
   let item = items[input]
+  // console.log(input)
   werewolf.inventory.push(item);
 }
-
-function addMods() {
-
-}
-
