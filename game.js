6865cc529260e1dec77647b84450b24b9f6c95f3
Rolland <Rollandcampbell@msn.com>
2019-08-19 10:44:11 -0600

    it works!

--- game.js
+++ game.js
@@ -9,9 +9,9 @@ let items = {
   parry: { name: 'Parry', modifier: -10, description: 'MISSED ME' },
 }
 
-//later use
-let werewolf = {
-  name: "Werewolf",
+//later use, only using health for now
+let mirrorMonster = {
+  name: "Mirror Monster",
   health: 100,
   inventory: []
 }
@@ -24,10 +24,12 @@ let player = {
 
 //reset button set helth to 100, clear out array of item
 function reset() {
-  player.health = 100;
+  mirrorMonster.health = 100;
   console.log(player.inventory[0])
   player.inventory.length = 0;
-  console.log(player.inventory[0])
+  console.log(player.inventory)
+  let showItem = document.getElementById("itemPick")
+  showItem.innerHTML = "<br><b>Unbridled FURY!!</b>"
   update()
 }
 
@@ -49,19 +51,24 @@ function reset() {
 
 function update() {
   let decreaseHealth = document.getElementById("healthEnemy")
-  decreaseHealth.innerHTML = player.health.toString();
+  decreaseHealth.innerHTML = mirrorMonster.health.toString();
 }
 
 //combined attacks into a single function
 function attackButton(inputAttack) {
+
+  let itemDamage = 0;
+  if (player.inventory.length) {
+    itemDamage = player.inventory[0].modifier
+  }
   if (inputAttack == 'slap') {
-    player.health--;
+    mirrorMonster.health -= (1 + itemDamage);
     update()
   } else if (inputAttack == 'punch') {
-    player.health -= 5;
+    mirrorMonster.health -= (5 + itemDamage);
     update()
   } else if (inputAttack == 'kick') {
-    player.health -= 10;
+    mirrorMonster.health -= (10 + itemDamage);
     update()
   }
 }
@@ -69,14 +76,12 @@ function attackButton(inputAttack) {
 //move item from obj list to inventory
 function giveItem(input) {
   let item = items[input]
-  // console.log(input)
-  player.inventory.push(item);
+  let showItem = document.getElementById("itemPick")
+  player.inventory[0] = item;
+  showItem.innerHTML = `<br><b>${player.inventory[0].name}!!</b>`;
 }
 
-let i = 0
-let itemDamage = player.inventory[0].modifier
-
-// for (i = 0, i < itemDamage; i++) {
-//   player.health -= itemDamage
+// for (let i = 0, i < itemDamage; i++) {
+//   werewolf.health -= itemDamage
 //   update()
 // }
